835ba2589b08772c78eca41f728bd8e93397b039
Vladyslav Grytsun <gritsun@gritsun-mbp.local>
2014-05-13 00:41:19 -0700

    url2text updates

--- url2text.py
+++ url2text.py
@@ -1,6 +1,16 @@
 from stripogram import html2text
-from urllib2 import urlopen
 import sys
+import httplib2
+
 
 if __name__ == '__main__':
-    print html2text(urlopen(sys.argv[1]).read())
+    http = httplib2.Http()
+    headers, body = http.request(sys.argv[1])
+    ctype = headers['content-type']
+    charset = ctype[ctype.index('charset=')+8:]
+    body = body.decode('UTF-8')
+    try:
+        text = html2text(body.encode('utf8', 'ignore'))
+    except UnicodeDecodeError:
+        text = html2text(body)
+    print text
